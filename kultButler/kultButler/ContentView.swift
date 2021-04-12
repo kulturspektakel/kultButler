e277ba512a0f90efd49adee29a96c986aab62bfc
Gabriel Knoll <gabriel.knoll@icloud.com>
2021-04-12 23:53:27 +0200

    Basic page-structure

--- kultButler/kultButler/ContentView.swift
+++ kultButler/kultButler/ContentView.swift
@@ -11,9 +11,29 @@ struct ContentView: View {
 	@StateObject var network = Network.shared
 
 	var body: some View {
-		List {
-			ForEach(network.products) { product in
-				Text((product.emoji ?? "") + " " + product.name)
+		HStack(alignment: .top, spacing: 20) {
+			Divider()
+			VStack(alignment: .leading, spacing: 20) {
+				Text("Produktauswahl").font(Font.system(.largeTitle))
+				List {
+					ForEach(network.products) { product in
+						Text((product.emoji ?? "") + " " + product.name)
+					}
+				}.listStyle(PlainListStyle())
+			}
+			Divider()
+			VStack(alignment: .leading) {
+				Text("Bestellung").font(Font.system(.largeTitle))
+				List {
+					Text("Produkt 1")
+					Text("Produkt 2")
+					Button("Bezahlen", action: { print("OK") })
+				}
+				.listStyle(SidebarListStyle())
+				.frame(minWidth: 0,
+					   maxWidth: UIScreen.main.bounds.width / 3,
+					   minHeight: 0,
+					   maxHeight: .infinity)
 			}
 		}.onAppear(perform: network.loadProducts)
 	}
