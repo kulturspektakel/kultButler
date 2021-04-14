bafeb0513103e09249765f94300c2aba4166e31d
tinolan <39703888+tinolan@users.noreply.github.com>
2021-04-14 15:30:38 +0200

    PayMethodsButton

--- kultButler/kultButler/PayMethodsView.swift
+++ kultButler/kultButler/PayMethodsView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct PayMethodsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button("Bar", action: { print("PayMethod: Bar") })
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(10.0)
+            Button("Karte", action: { print("PayMethod: Karte") })
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(10.0)
+            Button("Gutschein", action: { print("PayMethod: Gutschein") })
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(10.0)
+            Button("Orga", action: { print("PayMethod: Orga") })
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(10.0)
+        }
     }
 }
 
