4eb8a51b7e724c4b98984711631b93b32ba579e9
Gabriel Knoll <gabriel.knoll@icloud.com>
2021-04-14 14:15:39 +0200

    Added NavigationView

--- kultButler/kultButler/ContentView.swift
+++ kultButler/kultButler/ContentView.swift
@@ -11,32 +11,43 @@ struct ContentView: View {
 	@StateObject var network = Network.shared
 
 	var body: some View {
-		HStack(alignment: .top, spacing: 20) {
-			Divider()
-			VStack(alignment: .leading, spacing: 20) {
-				Text("Produktauswahl").font(Font.system(.largeTitle))
-				List {
-					ForEach(network.booths) { booth in
-						Text((booth.emoji ?? "") + " " + booth.name)
-						ProductView(products: booth.products)
-					}
-				}.listStyle(PlainListStyle())
-			}
-			Divider()
-			VStack(alignment: .leading) {
-				Text("Bestellung").font(Font.system(.largeTitle))
-				List {
-					Text("Produkt 1")
-					Text("Produkt 2")
-					Button("Bezahlen", action: { print("OK") })
+		NavigationView {
+			HStack(alignment: .top, spacing: 20) {
+				Divider()
+				VStack(alignment: .leading, spacing: 20) {
+					Text("Produktauswahl").font(Font.system(.largeTitle))
+					List {
+						ForEach(network.booths) { booth in
+							Text((booth.emoji ?? "") + " " + booth.name)
+							ProductView(products: booth.products)
+						}
+					}.listStyle(PlainListStyle())
 				}
-				.listStyle(SidebarListStyle())
-				.frame(minWidth: 0,
-					   maxWidth: UIScreen.main.bounds.width / 3,
-					   minHeight: 0,
-					   maxHeight: .infinity)
-			}
-		}.onAppear(perform: network.loadProducts)
+				Divider()
+
+				VStack(alignment: .leading) {
+					Text("Bestellung").font(Font.system(.largeTitle))
+					VStack {
+						Text("Produkt 1")
+						Text("Produkt 2")
+						NavigationLink(destination: TestView()) {
+							Text("Bezahlen")
+						}.navigationTitle("Navigation")
+					}
+					.listStyle(SidebarListStyle())
+					.frame(minWidth: 0,
+						   maxWidth: UIScreen.main.bounds.width / 3,
+						   minHeight: 0,
+						   maxHeight: .infinity)
+				}
+			}.onAppear(perform: network.loadProducts)
+		}.navigationViewStyle(StackNavigationViewStyle())
+	}
+}
+
+struct TestView: View {
+	var body: some View {
+		Text("Hello?")
 	}
 }
 
