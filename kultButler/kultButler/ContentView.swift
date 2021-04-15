bef7fb469b1cd5d26c926e6334299161d879c49d
tristanhaeuser <tristanhaeuser@icloud.com>
2021-04-15 16:08:18 +0200

    Created SelectedProductsView

--- kultButler/kultButler/ContentView.swift
+++ kultButler/kultButler/ContentView.swift
@@ -8,44 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-	@StateObject var network = Network.shared
+    @StateObject var network = Network.shared
 
-	var body: some View {
-		NavigationView {
-			HStack(alignment: .top, spacing: 20) {
-				Divider()
-				VStack(alignment: .leading, spacing: 20) {
-					Text("Produktauswahl").font(Font.system(.largeTitle))
-					List {
-						ForEach(network.booths) { booth in
-							Text((booth.emoji ?? "") + " " + booth.name)
-							ProductView(products: booth.products)
-						}
-					}.listStyle(PlainListStyle())
-				}
-				Divider()
+    var body: some View {
+        NavigationView {
+            HStack(alignment: .top, spacing: 20) {
+                Divider()
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Produktauswahl").font(Font.system(.largeTitle))
+                    List {
+                        ForEach(network.booths) { booth in
+                            Text((booth.emoji ?? "") + " " + booth.name)
+                            ProductView(products: booth.products)
+                        }
+                    }.listStyle(PlainListStyle())
+                }
+                Divider()
 
-				VStack(alignment: .leading) {
-					Text("Bestellung").font(Font.system(.largeTitle))
-					VStack {
-						Text("Produkt 1")
-						Text("Produkt 2")
-						NavigationLink(destination: PayMethodsView()) {
-							Text("Bezahlen")
+                VStack(alignment: .leading) {
+                    Text("Bestellung").font(Font.system(.largeTitle))
+                    VStack {
+                        Text("Produkt 1")
+                        Text("Produkt 2")
+                        NavigationLink(destination: PayMethodsView()) {
+                            Text("Bezahlen")
                                 .frame(width: 200, height: 100)
                                 .background(Color.blue)
                                 .foregroundColor(.white)
-						}.navigationTitle("Navigation")
-					}
-					.listStyle(SidebarListStyle())
-					.frame(minWidth: 0,
-						   maxWidth: UIScreen.main.bounds.width / 3,
-						   minHeight: 0,
-						   maxHeight: .infinity)
-				}
-			}.onAppear(perform: network.loadProducts)
-		}.navigationViewStyle(StackNavigationViewStyle())
-	}
+                        }.navigationTitle("Navigation")
+                    }
+                    .listStyle(SidebarListStyle())
+                    .frame(minWidth: 0,
+                           maxWidth: UIScreen.main.bounds.width / 3,
+                           minHeight: 0,
+                           maxHeight: .infinity)
+                }
+            }.onAppear(perform: network.loadProducts)
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ kultButler/kultButler/SelectedProductsView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectedProductsView.swift
+//  kultButler
+//
+//  Created by Tristan Häuser on 15.04.21.
+//
+
+import SwiftUI
+
+struct SelectedProductsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectedProductsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedProductsView()
+    }
+}
