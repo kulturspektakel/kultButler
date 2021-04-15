c768e914d8d793c6b84e0427c69cf164860ff25c
tristanhaeuser <tristanhaeuser@icloud.com>
2021-04-15 03:32:28 +0200

    Updated Product View

--- kultButler/kultButler/ProductView.swift
+++ kultButler/kultButler/ProductView.swift
@@ -9,40 +9,42 @@ import SwiftUI
 
 struct ProductView: View {
     var products = [Product]()
-
-    init(products: [Product]) {
+    init (products: [Product]) {
         self.products = products
     }
-
-    var colums: [GridItem] = Array(repeating: .init(.flexible()), count: 5)
+    let gridItemWidth = (UIScreen.main.bounds.width / 3) * 0.22
+    let gridSpacingWidth = (UIScreen.main.bounds.width / 3) * 0.05
+    var colums: [GridItem] = Array(repeating: .init(.fixed((UIScreen.main.bounds.width / 3) * 0.22 + (((UIScreen.main.bounds.width / 3) * 0.22) / 5) * 2), spacing: (UIScreen.main.bounds.width / 3) * 0.05), count: 5)
     var body: some View {
         ScrollView(.vertical) {
-            LazyVGrid(columns: colums, alignment: .leading, spacing: 20.0) {
-                ForEach(products) { product in
-                    VStack {
-                        // swiftlint:disable:next multiple_closures_with_trailing_closure
-                        Button( action: { print(product.name) }) {
-                            VStack {
-                                Text(product.name)
-                                    .fontWeight(.semibold)
-                                    .truncationMode(.tail)
-                                    .lineLimit(1)
-                                Text("")
-                                Text(String(Double(product.price) / 100) + "0€")
+            VStack {
+                LazyVGrid(columns: colums, alignment: .leading, spacing: gridSpacingWidth) {
+                    ForEach(products) { product in
+                        VStack {
+                            // swiftlint:disable:next multiple_closures_with_trailing_closure
+                            Button( action: { print(product.name) }) {
+                                VStack {
+                                    Text(product.name)
+                                        .fontWeight(.semibold)
+                                        .truncationMode(.tail)
+                                        .lineLimit(1)
+                                    Text("")
+                                    Text("\(priceAsDouble(price: product.price), specifier: "%.2f") €")
+                                }
                             }
+                            .frame(width: gridItemWidth, height: gridItemWidth, alignment: .center)
+                            .padding((gridItemWidth / 5))
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(gridItemWidth / 4.5)
                         }
-                        .frame(minWidth: 100, maxWidth: 100, minHeight: 100, maxHeight: 100, alignment: .center)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(20)
                     }
                 }
             }
         }
     }
+    func priceAsDouble(price: Int) -> Double { (Double(price) / 100.0) }
 }
-
 struct ProductView_Previews: PreviewProvider {
     static var previews: some View {
         let products = [Product(name: "Fleischlappen", price: 400)]
